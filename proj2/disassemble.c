61b5c1c47e3ef056aba2d1285bb099d15ab57771
Benjamin Han <benjamin.han90@gmail.com>
2012-02-18 15:21:13 -0800

    Proj2-1

--- proj2/disassemble.c
+++ proj2/disassemble.c
@@ -12,14 +12,62 @@ void disassemble(inst_t inst)
   case 0x0: // opcode == 0x0 (SPECIAL)
 
     switch (inst.rtype.funct)
-    {
+    {	
+    case 0x0: // funct == 0x0 (sll)
+	printf("sll\t$%d,$%d,%u\n", inst.rtype.rd, inst.rtype.rt, inst.rtype.shamt);
+	break;
+	
+    case 0x2: // funct == 0x2 (srl)
+	printf("srl\t$%d,$%d,%u\n", inst.rtype.rd, inst.rtype.rt, inst.rtype.shamt);
+	break;
+
+    case 0x3: // funct == 0x3 (sra)
+	printf("sra\t$%d,$%d,%u\n", inst.rtype.rd, inst.rtype.rt, inst.rtype.shamt);
+	break;
+
+    case 0x8: // funct == 0x8 (jr)
+	printf("jr\t$%d\n", inst.rtype.rs);
+	break;
+
+    case 0x9: // funct == 0x9 (jalr)
+	printf("jalr\t$%d,%d\n", inst.rtype.rd, inst.rtype.rs);
+	break;
+	
     case 0xc: // funct == 0xc (SYSCALL)
       printf("syscall\n");
       break;
 
+    case 0x21: // funct == 0x21 (addu)
+	printf("addu\t$%d,$%d,$%d\n", inst.rtype.rd, inst.rtype.rs, inst.rtype.rt);
+	break;
+
+    case 0x23: // funct == 0x23 (subu)
+	printf("subu\t$%d,$%d,$%d\n", inst.rtype.rd, inst.rtype.rs, inst.rtype.rt);
+	break;
+
+    case 0x24: // funct == 0x24 (and)
+	printf("and\t$%d,$%d,$%d\n", inst.rtype.rd, inst.rtype.rs, inst.rtype.rt);
+	break;
+      
     case 0x25: // funct == 0x25 (OR)
       printf("or\t$%d,$%d,$%d\n", inst.rtype.rd, inst.rtype.rs, inst.rtype.rt);
       break;
+
+    case 0x26: // funct == 0x26 (xor)
+	printf("xor\t$%d,$%d,$%d\n", inst.rtype.rd, inst.rtype.rs, inst.rtype.rt);
+	break;
+
+    case 0x27: // funct == 0x27 (nor)
+	printf("nor\t$%d,$%d,$%d\n", inst.rtype.rd, inst.rtype.rs, inst.rtype.rt);
+	break;
+
+    case 0x2a: // funct == 0x2a (slt)
+	printf("slt\t$%d,$%d,$%d\n", inst.rtype.rd, inst.rtype.rs, inst.rtype.rt);
+	break;
+
+    case 0x2b: // funct == 0x2b (sltu)
+	printf("sltu\t$%d,$%d,$%d\n", inst.rtype.rd, inst.rtype.rs, inst.rtype.rt);
+	break;
 
     default: // undefined funct
       fprintf(stderr, "%s: illegal instruction: %08x\n", __FUNCTION__, inst.bits);
@@ -27,15 +75,83 @@ void disassemble(inst_t inst)
       break;
     }
     break;
+    
+  case 0x2: // opcode == 0x2 (J)
+    printf("j\t0x%x\n", inst.jtype.addr*4);
+    break;
+
+  case 0x3: // opcode == 0x3 (jal)
+      printf("jal\t0x%x\n", 4 * inst.jtype.addr);
+      break;
+
+  case 0x4: // opcode == 0x4 (beq)
+      printf("beq\t$%d,$%d,%d\n", inst.itype.rs, inst.itype.rt, 4 * inst.itype.imm);
+      break;
+
+  case 0x5: // opcode == 0x5 (bne)
+      printf("bne\t$%d,$%d,%d\n", inst.itype.rs, inst.itype.rt, 4 * inst.itype.imm);
+      break;
+
+  case 0x9: // opcode == 0x9 (addiu)
+      printf("addiu\t$%d,$%d,%d\n", inst.itype.rt, inst.itype.rs, inst.itype.imm);
+      break;
+
+  case 0xa: // opcode == 0xa (slti)
+      printf("slti\t$%d,$%d,0x%x\n", inst.itype.rt, inst.itype.rs, inst.itype.imm);
+      break;
+
+  case 0xb: // opcode == 0xb (sltiu)
+      printf("sltiu\t$%d,$%d,0x%x\n", inst.itype.rt, inst.itype.rs, inst.itype.imm);
+      break;
+
+  case 0xc: // opcode == 0xc (andi)
+      printf("andi\t$%d,$%d,0x%x\n", inst.itype.rt, inst.itype.rs, inst.itype.imm);
+      break;
 
   case 0xd: // opcode == 0xd (ORI)
     printf("ori\t$%d,$%d,0x%x\n", inst.itype.rt, inst.itype.rs, inst.itype.imm);
     break;
 
-  case 0x2: // opcode == 0x2 (J)
-    printf("j\t0x%x\n", inst.jtype.addr*4);
-    break;
+  case 0xe: // opcode == 0xe (xori)
+      printf("xori\t$%d,$%d,0x%x\n", inst.itype.rt, inst.itype.rs, inst.itype.imm);
+      break;
 
+  case 0xf: // opcode == 0xf (lui)
+      printf("lui\t$%d,0x%x\n", inst.itype.rt, inst.itype.imm);
+      break;
+
+  case 0x20: // opcode == 0x20 (lb)
+      printf("lb\t$%d,%d($%d)\n", inst.itype.rt, 4 * inst.itype.imm, inst.itype.rs);
+      break;
+
+  case 0x21: // opcode == 0x21 (lh)
+      printf("lh\t$%d,%d($%d)\n", inst.itype.rt, 4 * inst.itype.imm, inst.itype.rs);
+      break;
+
+  case 0x23: // opcode == 0x23 (lw)
+      printf("lw\t$%d,%d($%d)\n", inst.itype.rt, 4 * inst.itype.imm, inst.itype.rs);
+      break;
+
+  case 0x24: // opcode == 0x24 (lbu)
+      printf("lbu\t$%d,%d($%d)\n", inst.itype.rt, 4 * inst.itype.imm, inst.itype.rs);
+      break;
+
+  case 0x25: // opcode == 0x25 (lhu)
+      printf("lhu\t$%d,%d($%d)\n", inst.itype.rt, 4 * inst.itype.imm, inst.itype.rs);
+      break;
+
+  case 0x28: // opcode == 0x28 (sb)
+      printf("sb\t$%d,%d($%d)\n", inst.itype.rt, 4 * inst.itype.imm, inst.itype.rs);
+      break;
+
+  case 0x29: // opcode == 0x29 (sh)
+      printf("sh\t$%d,%d($%d)\n", inst.itype.rt, 4 * inst.itype.imm, inst.itype.rs);
+      break;
+      
+  case 0x2b: // opcode == 0x2b (sw)
+      printf("sw\t$%d,%d($%d)\n", inst.itype.rt, 4 * inst.itype.imm, inst.itype.rs);
+      break;
+    
   default: // undefined opcode
     fprintf(stderr, "%s: illegal instruction: %08x\n", __FUNCTION__, inst.bits);
     exit(-1);
--- proj2/processor.c
+++ proj2/processor.c
@@ -29,15 +29,76 @@ void execute_one_inst(processor_t* p, int prompt, int print_regs)
 
     switch (inst.rtype.funct)
     {
+		
+    case 0x0: // funct == 0x0 (sll)
+	//finish
+	p->pc += 4;
+	break;
+	
+    case 0x2: // funct == 0x2 (srl)
+	//finish
+	p->pc += 4;
+	break;
+
+    case 0x3: // funct == 0x3 (sra)
+	//finish
+	p->pc += 4;
+	break;
+
+    case 0x8: // funct == 0x8 (jr)
+	//finish
+	p->pc += 4;
+	break;
+
+    case 0x9: // funct == 0x9 (jalr)
+	//finish
+	p->pc += 4;
+	break;	
+	
     case 0xc: // funct == 0xc (SYSCALL)
       handle_syscall(p);
       p->pc += 4;
       break;
 
+    case 0x21: // funct == 0x21 (addu)
+	//finish
+	p->pc += 4;
+	break;
+
+    case 0x23: // funct == 0x23 (subu)
+	//finish
+	p->pc += 4;
+	break;
+
+    case 0x24: // funct == 0x24 (and)
+	//finish
+	p->pc += 4;
+	break;
+      
     case 0x25: // funct == 0x25 (OR)
       p->R[inst.rtype.rd] = p->R[inst.rtype.rs] | p->R[inst.rtype.rt];
       p->pc += 4;
       break;
+
+    case 0x26: // funct == 0x26 (xor)
+	//finish
+	p->pc += 4;
+	break;
+
+    case 0x27: // funct == 0x27 (nor)
+	//finish
+	p->pc += 4;
+	break;
+
+    case 0x2a: // funct == 0x2a (slt)
+	//finish
+	p->pc += 4;
+	break;
+
+    case 0x2b: // funct == 0x2b (sltu)
+	//finish
+	p->pc += 4;
+	break;
 
     default: // undefined funct
       fprintf(stderr, "%s: pc=%08x, illegal instruction=%08x\n", __FUNCTION__, p->pc, inst.bits);
@@ -45,16 +106,101 @@ void execute_one_inst(processor_t* p, int prompt, int print_regs)
       break;
     }
     break;
+    
+  case 0x2: // opcode == 0x2 (J)
+    p->pc = ((p->pc+4) & 0xf0000000) | (inst.jtype.addr << 2);
+    break;
+
+  case 0x3: // opcode == 0x3 (jal)
+      //finish
+      p->pc += 4;
+      break;
+
+  case 0x4: // opcode == 0x4 (beq)
+      //finish
+      p->pc += 4;
+      break;
+
+  case 0x5: // opcode == 0x5 (bne)
+      //finish
+      p->pc += 4;
+      break;
+
+  case 0x9: // opcode == 0x9 (addiu)
+      //finish
+      p->pc += 4;
+      break;
+
+  case 0xa: // opcode == 0xa (slti)
+      //finish
+      p->pc += 4;
+      break;
+
+  case 0xb: // opcode == 0xb (sltiu)
+      //finish
+      p->pc += 4;
+      break;
+
+  case 0xc: // opcode == 0xc (andi)
+      //finish
+      p->pc += 4;
+      break;
 
   case 0xd: // opcode == 0xd (ORI)
     p->R[inst.itype.rt] = p->R[inst.itype.rs] | inst.itype.imm;
     p->pc += 4;
     break;
 
-  case 0x2: // opcode == 0x2 (J)
-    p->pc = ((p->pc+4) & 0xf0000000) | (inst.jtype.addr << 2);
-    break;
+  case 0xe: // opcode == 0xe (xori)
+      //finish
+      p->pc += 4;
+      break;
 
+  case 0xf: // opcode == 0xf (lui)
+      //finish
+      p->pc += 4;
+      break;
+
+  case 0x20: // opcode == 0x20 (lb)
+      //finish
+      p->pc += 4;
+      break;
+
+  case 0x21: // opcode == 0x21 (lh)
+      //finish
+      p->pc += 4;
+      break;
+
+  case 0x23: // opcode == 0x23 (lw)
+      //finish
+      p->pc += 4;
+      break;
+
+  case 0x24: // opcode == 0x24 (lbu)
+      //finish
+      p->pc += 4;
+      break;
+
+  case 0x25: // opcode == 0x25 (lhu)
+      //finish
+      p->pc += 4;
+      break;
+
+  case 0x28: // opcode == 0x28 (sb)
+      //finish
+      p->pc += 4;
+      break;
+
+  case 0x29: // opcode == 0x29 (sh)
+      //finish
+      p->pc += 4;
+      break;
+      
+  case 0x2b: // opcode == 0x2b (sw)
+      //finish
+      p->pc += 4;
+      break;
+     
   default: // undefined opcode
     fprintf(stderr, "%s: pc=%08x, illegal instruction: %08x\n", __FUNCTION__, p->pc, inst.bits);
     exit(-1);
