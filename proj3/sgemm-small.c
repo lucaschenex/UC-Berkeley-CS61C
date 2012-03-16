9e1b74f1da2cf601e8a6819f9e3f9c651b888e14
Hairan Zhu <cubrikal@gmail.com>
2012-03-15 18:39:31 -0700

    n/a

--- proj3/sgemm-small.c
+++ proj3/sgemm-small.c
@@ -13,7 +13,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void transpose(int n, int blocksize, float *src, float *dst) {
+void transpose(int n, float *src, float *dst) {
+    const int blocksize = 400;
     int i,j,k,m;
     for (i = 0; i < n; i += blocksize)
         for (j = 0; j < n; j += blocksize)
@@ -22,36 +23,18 @@ void transpose(int n, int blocksize, float *src, float *dst) {
                     dst[m + k*n] = src[k + m*n];
 }
 
-/*
-void square_sgemm(int n, float* A, float* B, float* C) {
-    float* AT = malloc(n * n * sizeof(float));
-    int i, j, k;
-    float cij;
-    transpose(n, 300, A, AT);
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) { 
-            cij = C[i+j*n];
-            for (k = 0; k < n; k++)
-                cij += AT[k+i*n] * B[k+j*n];
-        C[i+j*n] = cij;
-        }
-    }
-    free(AT);
-}
-*/
-
 /* This routine performs a sgemm operation
  *  C := C + A * B
  * where A, B, and C are lda-by-lda matrices stored in column-major format.
  * On exit, A and B maintain their input values. */    
 void square_sgemm (int n, float *A, float *B, float *C)
 {
-    const int blocksize = 4, t_blocksize = 400, stride = 8;
+    const int blocksize = 4, stride = 16;
     int i, j, k, i2, j2;
-    __m128 mmA, mmB, mmC, mmSum;
+    __m128 mmA, mmB, mmC, mmSum1, mmSum2, mmSum3, mmSum4;
     
     float *AT = malloc(n * n * sizeof(float));
-    transpose(n, t_blocksize, A, AT);
+    transpose(n, A, AT);
     
     float buffer[4] = {0};
     
@@ -59,18 +42,35 @@ void square_sgemm (int n, float *A, float *B, float *C)
         for (j = 0; j < n; j += blocksize) {
             for (i2 = i; (i2 < i + blocksize) && (i2 < n); i2++) {
                 for (j2 = j; (j2 < j + blocksize) && (j2 < n); j2++) {
-                    mmSum = _mm_setzero_ps();
+                    mmSum1 = _mm_setzero_ps();
+                    mmSum2 = _mm_setzero_ps();
+                    mmSum3 = _mm_setzero_ps();
+                    mmSum4 = _mm_setzero_ps();
                     for (k = 0; k < (n / stride) * stride; k += stride) {
                         mmA = _mm_loadu_ps(AT + i2*n + k);
                         mmB = _mm_loadu_ps(B + k + j2*n);
                         mmA = _mm_mul_ps(mmA, mmB);
-                        mmSum = _mm_add_ps(mmSum, mmA);
+                        mmSum1 = _mm_add_ps(mmSum1, mmA);
+                    
                         mmA = _mm_loadu_ps(AT + i2*n + k + 4);
                         mmB = _mm_loadu_ps(B + k + j2*n + 4);
                         mmA = _mm_mul_ps(mmA, mmB);
-                        mmSum = _mm_add_ps(mmSum, mmA);
-                    }
-                    _mm_storeu_ps(buffer, mmSum);
+                        mmSum2 = _mm_add_ps(mmSum2, mmA);
+                    
+                        mmA = _mm_loadu_ps(AT + i2*n + k + 8);
+                        mmB = _mm_loadu_ps(B + k + j2*n + 8);
+                        mmA = _mm_mul_ps(mmA, mmB);
+                        mmSum3 = _mm_add_ps(mmSum3, mmA);
+                    
+                        mmA = _mm_loadu_ps(AT + i2*n + k + 12);
+                        mmB = _mm_loadu_ps(B + k + j2*n + 12);
+                        mmA = _mm_mul_ps(mmA, mmB);
+                        mmSum4 = _mm_add_ps(mmSum4, mmA);
+                    }    
+                    mmSum1 = _mm_add_ps(mmSum1, mmSum2);
+                    mmSum3 = _mm_add_ps(mmSum3, mmSum4);
+                    mmSum1 = _mm_add_ps(mmSum1, mmSum3);
+                    _mm_storeu_ps(buffer, mmSum1);
                     C[i2 + j2*n] += buffer[0] + buffer[1] + buffer[2] + buffer[3];
                     for (k = (n / stride * stride); k < n; k++) {
                         C[i2 + j2*n] += AT[i2*n + k] * B[k + j2*n];
