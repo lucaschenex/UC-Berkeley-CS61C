cae370beb95d42c993407ab782b9821bd736f622
Benjamin Han <benjamin.han90@gmail.com>
2012-03-15 21:57:29 -0700

    Trying to pad

--- proj3/sgemm-small.c
+++ proj3/sgemm-small.c
@@ -13,77 +13,73 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/* Matrix transpose. */
-void transpose(int n, float *src, float *dst) {
-    const int blocksize = 100;
-    int i,j,k,m;
-    for (i = 0; i < n; i += blocksize)
-        for (j = 0; j < n; j += blocksize)
-            for (m = j; (m < j + blocksize) && (m < n); m++)
-                for (k = i; (k < i + blocksize) && (k < n); k++)
-                    dst[m + k*n] = src[k + m*n];
+/* Converts matrix B to column-major to reduce stride */
+float* transpose(int n, int paddedSize, int blocksize, float *B, float*dst) {
+    for ( int i = 0; i < n; i += blocksize ) 	
+	for ( int j = 0; j < n; j += blocksize ) 
+	    for ( int k = i; (k < (i + blocksize)) && (k < n); k++ ) 
+		for ( int m = j; (m < (j + blocksize)) && (m < n); m++ ) {
+		    dst[m*paddedSize + k] = B[m + k*n];
+		}
 }
 
+/* Pads A to ignore matrix sizes. intrinsics? */
+float* pad(int n, int paddedSize, int blocksize, float *A, float *dst) {
+    for ( int i = 0; i < n; i += blocksize ) 	
+	for ( int j = 0; j < n; j += blocksize ) 
+	    for ( int k = i; (k < (i + blocksize)) && (k < n); k++ ) 
+		for ( int m = j; (m < (j + blocksize)) && (m < n); m++ ) {
+		    dst[m + k*paddedSize] = A[m + k*n];
+		}
+}
+
+/* Unpads C to restore matrix sizes. intrinsics? */
+float* unpad(int n, int paddedSize, int blocksize, float *cTmp, float *dst) {
+    for ( int i = 0; i < n; i += blocksize ) 	
+	for ( int j = 0; j < n; j += blocksize ) 
+	    for ( int k = i; (k < (i + blocksize)) && (k < n); k++ ) 
+		for ( int m = j; (m < (j + blocksize)) && (m < n); m++ ) {
+		    dst[m + k*n] = cTmp[m + k*paddedSize];
+		}
+}
+
+
 /* This routine performs a sgemm operation
- * C := C + A * B
+ *  C := C + A * B
  * where A, B, and C are lda-by-lda matrices stored in column-major format.
  * On exit, A and B maintain their input values. */    
 void square_sgemm (int n, float* A, float* B, float* C)
 {
-    int k, j, k2, j2, i;
-    const int blocksize = 16;
-    __m128 mmA, mmB, mmC;
+    int blocksize = 16;
+    int paddedSize = n;
+    if (paddedSize % blocksize > 0) paddedSize += (blocksize - (paddedSize % blocksize));
+    float *bTmp = calloc(paddedSize*paddedSize, sizeof(float));
+    transpose(n, paddedSize, blocksize, B, bTmp);
+    float *aTmp = calloc(paddedSize*paddedSize, sizeof(float));
+    pad(n, paddedSize, blocksize, A, aTmp);
+    float *cTmp = calloc(paddedSize*paddedSize, sizeof(float));
+
     
-    //convert B to column major
-    float *BT = malloc(n * n * sizeof(float));
-    transpose(n, B, BT);
-    B = BT;
+    __m128 mmA, mmB, mmC;
+    int k, j, k2, j2, i;
+    for (k = 0; k < paddedSize; k += 4)
+    	for (j = 0; j < paddedSize; j += 4) {
+	    mmC = _mm_loadu_ps(cTmp + i + j*n);
+	    mmB = _mm_load1_ps(bTmp + k*n + j);
+	    mmA = _mm_loadu_ps(aTmp + i + k*n);
+	    _mm_storeu_ps(cTmp + i + j*n, _mm_add_ps(mmC, _mm_mul_ps(mmA, mmB)));
+	    /* mmA = _mm_loadu_ps(aTmp + i + k*n + 4); */
+	    /* mmC = _mm_loadu_ps(cTmp + i + j*n + 4); */
+	    /* _mm_storeu_ps(cTmp + i + j*n + 4, _mm_add_ps(mmC, _mm_mul_ps(mmA, mmB))); */
+	    /* mmA = _mm_loadu_ps(aTmp + i + k*n + 8); */
+	    /* mmC = _mm_loadu_ps(cTmp + i + j*n + 8); */
+	    /* _mm_storeu_ps(cTmp + i + j*n + 8, _mm_add_ps(mmC, _mm_mul_ps(mmA, mmB))); */
+	    /* mmA = _mm_loadu_ps(aTmp + i + k*n + 12); */
+	    /* mmC = _mm_loadu_ps(cTmp + i + j*n + 12); */
+	    /* _mm_storeu_ps(cTmp + i + j*n + 12, _mm_add_ps(mmC, _mm_mul_ps(mmA, mmB))); */
+	}
 
-    for (k = 0; k < n; k += blocksize)
-    	for (j = 0; j < n; j += blocksize)
-    	    for (k2 = k; (k2 < k + blocksize) && (k2 < n); k2++) {
-                for (j2 = j; (j2 < j + blocksize) && (j2 < n); j2++) {
-                    float bkj = B[k2 * n + j2];
-                    mmB = _mm_load1_ps(&bkj);
-                    for (i = 0; i < (n & ~0xf); i += 16) {
-                        mmC = _mm_loadu_ps(C + i + j2*n);
-                        mmA = _mm_loadu_ps(A + i + k2*n);
-                        mmA = _mm_mul_ps(mmA, mmB);
-                        mmC = _mm_add_ps(mmC, mmA);
-                        _mm_storeu_ps(C + i + j2*n, mmC);
-                        
-                        mmC = _mm_loadu_ps(C + i + j2*n + 4);
-                        mmA = _mm_loadu_ps(A + i + k2*n + 4);
-                        mmA = _mm_mul_ps(mmA, mmB);
-                        mmC = _mm_add_ps(mmC, mmA);
-                        _mm_storeu_ps(C + i + j2*n + 4, mmC);
+    unpad(n, paddedSize, blocksize, cTmp, C);
 
-                        mmC = _mm_loadu_ps(C + i + j2*n + 8);
-                        mmA = _mm_loadu_ps(A + i + k2*n + 8);
-                        mmA = _mm_mul_ps(mmA, mmB);
-                        mmC = _mm_add_ps(mmC, mmA);
-                        _mm_storeu_ps(C + i + j2*n + 8, mmC);
-                        
-                        mmC = _mm_loadu_ps(C + i + j2*n + 12);
-                        mmA = _mm_loadu_ps(A + i + k2*n + 12);
-                        mmA = _mm_mul_ps(mmA, mmB);
-                        mmC = _mm_add_ps(mmC, mmA);
-                        _mm_storeu_ps(C + i + j2*n + 12, mmC);
-                    }
-
-                    for (i = n & ~0xf; i < (n & ~0x3); i += 4) {
-                        mmC = _mm_loadu_ps(C + i + j2*n);
-                        mmA = _mm_loadu_ps(A + i + k2*n);
-                        mmA = _mm_mul_ps(mmA, mmB);
-                        mmC = _mm_add_ps(mmC, mmA);
-                        _mm_storeu_ps(C + i + j2*n, mmC);
-                    }
-
-                    for(i = n & ~0x3; i < n; i++)
-                        C[i + j2*n] += A[i + k2*n] * bkj; 
-    		}
-        }
-
-    free(BT);
+    free(aTmp); free(bTmp); free(cTmp);
 }
-
