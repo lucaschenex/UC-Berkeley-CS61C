3e9bdda68c272fe99e71df01ff743679e593b75f
Hairan Zhu <cubrikal@gmail.com>
2012-03-17 14:18:48 -0700

    explicit 64

--- proj3/sgemm-small.c
+++ proj3/sgemm-small.c
@@ -27,19 +27,29 @@ void unpad(int n, int padded_size, float *cTmp, float *dst) {
         memcpy(dst + i*n, cTmp + i*padded_size, n * sizeof(float));
 }
 
+
+
+
+
+void square64_sgemm (float *A, float *B, float *C);
+
 /* This routine performs a sgemm operation
  *  C := C + A * B
  * where A, B, and C are lda-by-lda matrices stored in column-major format.
  * On exit, A and B maintain their input values. */    
 void square_sgemm (int n, float *A, float *B, float *C)
 {
+    if (n == 64) {
+        square64_sgemm(A, B, C);
+        return;
+    }
+
     const int stride = 16;
     const int padded_size = (n + stride - 1) & ~(stride - 1);
 
     float *Acpy = calloc(padded_size * padded_size, sizeof(float));
     float *Bcpy = calloc(padded_size * padded_size, sizeof(float));
     float *Ccpy = calloc(padded_size * padded_size, sizeof(float));
-    
     pad(n, padded_size, A, Acpy);
     pad(n, padded_size, B, Bcpy);
 
@@ -55,9 +65,52 @@ void square_sgemm (int n, float *A, float *B, float *C)
                     }
 
     unpad(n, padded_size, Ccpy, C);
+}
 
-    free(Acpy);
-    free(Bcpy);
-    free(Ccpy);
+
+/** Hard coded for 64. */
+void square64_sgemm (float *A, float *B, float *C)
+{
+    __m128 mmA1, mmA2, mmB1, mmB2, mmC1, mmC2, mmSum1, mmSum2;
+    
+    /* Transpose A. */
+    float *AT = calloc(64 * 64, sizeof(float));
+    for (int i = 0; i < 64; i += 16)
+        for (int j = 0; j < 64; j += 16)
+            for (int i2 = i; i2 < i + 16; i2++)
+                for (int j2 = j; j2 < j + 16; j2++)
+                    AT[i2 + j2*64] = A[j2 + i2*64];
+    
+    for (int i = 0; i < 64; i ++)
+        for (int j = 0; j < 64; j ++) {
+            mmSum1 = _mm_load_ss(C + i + j*64);
+            mmSum2 = _mm_setzero_ps();
+            for (int k = 0; k < 64; k += 16) {
+                mmA1 = _mm_loadu_ps(AT + i*64 + k);
+                mmB1 = _mm_loadu_ps(B + k + j*64);
+                mmA1 = _mm_mul_ps(mmA1, mmB1);
+                mmSum1 = _mm_add_ps(mmSum1, mmA1);
+            
+                mmA2 = _mm_loadu_ps(AT + i*64 + k + 4);
+                mmB2 = _mm_loadu_ps(B + k + j*64 + 4);
+                mmA2 = _mm_mul_ps(mmA2, mmB2);
+                mmSum2 = _mm_add_ps(mmSum2, mmA2);
+            
+                mmA1 = _mm_loadu_ps(AT + i*64 + k + 8);
+                mmB1 = _mm_loadu_ps(B + k + j*64 + 8);
+                mmA1 = _mm_mul_ps(mmA1, mmB1);
+                mmSum1 = _mm_add_ps(mmSum1, mmA1);
+            
+                mmA2 = _mm_loadu_ps(AT + i*64 + k + 12);
+                mmB2 = _mm_loadu_ps(B + k + j*64 + 12);
+                mmA2 = _mm_mul_ps(mmA2, mmB2);
+                mmSum2 = _mm_add_ps(mmSum2, mmA2);
+            }
+            mmSum1 = _mm_add_ps(mmSum1, mmSum2);
+            mmSum1 = _mm_hadd_ps(mmSum1, mmSum1);
+            mmSum1 = _mm_hadd_ps(mmSum1, mmSum1);
+            _mm_store_ss(C + i + j*64, mmSum1);
+        }
+    free(AT);
 }
 
